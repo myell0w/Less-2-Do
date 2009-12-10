a9c320e86559781c8d461b72d5a39471c9356b6e
Philip Messlehner <philip@philip-messlehners-macbook.local>
2009-12-10 11:07:06 +0100

    added EditFolderViewController

---
+++ Classes/EditFolderViewController.h
@@ -0,0 +1,30 @@
+//
+//  EditFolderViewController.h
+//  Less2Do
+//
+//  Created by Philip Messlehner on 08.12.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FolderFirstLevelController.h"
+
+
+@interface EditFolderViewController : UIViewController {
+	UITextField *_nameTextField;
+	Folder *_folder;   
+	FolderFirstLevelController *_parent;
+}
+
+@property (nonatomic, retain) IBOutlet UITextField *nameTextField;
+@property (nonatomic, retain) Folder *folder;
+@property (nonatomic, retain) FolderFirstLevelController *parent;
+
+//- (id)initWithStyle:(UITableViewStyle)aStyle andParent:(FolderFirstLevelController *)aParent andFolder:(Folder *)aFolder;
+//- (id)initWithStyle:(UITableViewStyle)aStyle andParent:(FolderFirstLevelController *)aParent;
+//- (UITableViewCell *)createNameCell;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil parent:(FolderFirstLevelController *)aParent folder:(Folder *)aFolder;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil parent:(FolderFirstLevelController *)aParent;
+- (IBAction)cancel:(id)sender;
+- (IBAction)save:(id)sender;
+@end
