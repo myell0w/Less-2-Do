38062241fd7b6c393cd738fe091cb597db3a1131
BlackandCold <johanneskurz@WaechterX.local>
2009-12-24 01:54:40 +0100

    Base Sync Object - here we go

---
+++ Classes/BaseSyncObject.m
@@ -0,0 +1,14 @@
+//
+//  BaseSyncObject.m
+//  Less2Do
+//
+//  Created by BlackandCold on 24.12.09.
+//  Copyright 2009 TU Wien. All rights reserved.
+//
+
+#import "BaseSyncObject.h"
+
+
+@implementation BaseSyncObject
+
+@end
