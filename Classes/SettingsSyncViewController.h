1bccf38d7a891a97019d56705179755c51769f81
Matthias <matthias.tretter@gmail.com>
2010-01-03 21:45:22 +0100

    started with Sync-Settings-GUI

---
+++ Classes/SettingsSyncViewController.h
@@ -0,0 +1,24 @@
+//
+//  SettingsSyncViewController.h
+//  Less2Do
+//
+//  Created by Matthias Tretter on 03.01.10.
+//  Copyright 2010 BIAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SettingsViewController.h"
+
+
+@interface SettingsSyncViewController : SettingsViewController {
+	UITextField *eMail;
+	UITextField *password;
+}
+
+@property (nonatomic, retain) IBOutlet UITextField *eMail;
+@property (nonatomic, retain) IBOutlet UITextField *password;
+
+- (IBAction)forceLocalToRemoteSync:(id)sender;
+- (IBAction)forceRemoteToLocalSync:(id)sender;
+- (IBAction)unlinkAccount:(id)sender;
+@end
