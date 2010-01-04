f1fed61ff297b8a8a865c30248bd0b85c935734e
Gerhard Schraml <gerhard@Gerhard-Schramls-MacBook-Pro.local>
2010-01-04 17:12:46 +0100

    First Sync Steps

---
+++ Classes/SyncManager.m
@@ -0,0 +1,25 @@
+//
+//  SyncManager.m
+//  Less2Do
+//
+//  Created by Gerhard Schraml on 04.01.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "SyncManager.h"
+
+
+@implementation SyncManager
+
+-(void)syncForceLocal:(NSError**)error
+{
+	/*
+	 1. 
+	*/
+}
+
+-(void)syncForceRemote:(NSError**)error
+{
+}
+
+@end
